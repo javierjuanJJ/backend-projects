9bc63a6e0ab3455eb64806da8a2c3ec74ff0dcff
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2026-03-04 13:14:16 +0100

    Add parameter to 2-github-user-activity

--- 2-github-user-activity/index.js
+++ 2-github-user-activity/index.js
@@ -1,6 +1,14 @@
 import { readdir, stat } from 'node:fs/promises'
 import { join } from 'node:path'
 
+const args = process.argv.slice(2) // Solo argumentos relevantes
+
+if (args.length !== 1) {
+    console.error('❌ Solo se puede poner un parametro')
+    process.exit(1)
+}
 
 // 1. Recuperar la carpeta a listar
-const githubUsername = process.argv[2] ?? '.'
+const githubUsername = args[0]
+
+
