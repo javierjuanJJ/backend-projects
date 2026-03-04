2a53674053b12d3435004f4ee6e0c9221546e682
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2026-03-04 14:03:38 +0100

    Add components to Unit-converter

---
+++ 5-unit-converter/unit-converter/app/components/Calculator.tsx
@@ -0,0 +1,15 @@
+export default function Home( {listConvertions} ) {
+    return (
+      <label htmlFor=""></label>
+      <input type="number" name="" id="" />
+
+      <label htmlFor=""></label>
+      <input type="checkbox" name="" id="" />
+
+      <label htmlFor=""></label>
+      <input type="checkbox" name="" id="" />
+
+
+      <input type="submit" value="" />
+    );
+  }
